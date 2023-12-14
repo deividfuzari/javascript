fb1d269c7c054bf404d2b88476ada524ff49feb1
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-14 19:50:16 -0300

    att exercicio

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+if (vel > 60) {  //condição simples
+    console.log('Voce está acima da velocidade!FOI MULTADO!! ')
+}
+console.log('dirija com cuidado')
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'França'
+console.log(`Voce está vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Voce é Brasileira')
+}           
+else {      // Condição composta pois tem if/else
+    console.log('Voce é Estrangeira')
+}
