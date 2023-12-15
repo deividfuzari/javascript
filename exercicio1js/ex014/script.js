5522346efa4695c5b9d439b80c1506c28e2fb130
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-15 19:53:41 -0300

    projeto horario

---
+++ exercicio1js/ex014/script.js
@@ -0,0 +1,27 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var imgContainer = window.document.getElementById('foto')
+    var data = new Date
+    //var hora = data.getHours()
+    var hora = 13
+    msg.innerText = `Agora são exatamente ${hora} horas.`
+    console.log(img)
+    if ( hora > 0 && hora <12){
+        //bom dia
+        img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
+    }else if (hora >=12 && hora < 18){
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+        imgContainer.style.paddingLeft = '-100px'
+        console.log(img.style)
+        
+    }else{
+        //boa noite
+        img.src= 'noite.png'
+        document.body.style.background = '#515154'
+    }
+}
+
