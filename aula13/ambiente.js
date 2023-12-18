d69c81ded4d731fe6b2a27590d469157c52c3158
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-18 13:06:58 -0300

    aprendendo

---
+++ aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+do {
+    console.log(`Bom dia, passo ${c}`)
+    c++
+}while (c <= 6) //usando o do while que tem o teste logico no final e o bloco primeiro.
+
+
+var c = 1
+while (c <= 6)
+    console.log(`Bom dia, passo ${c}`)
+    c++
+    //usando o while com o teste logico primeiro e o bloco caso for verdadeiro o teste logico.
+ 
+
+    
---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++ 
+}
+
+ */
+
+for(var c = 1;c <= 4; c++){
+    console.log(c)
+}
