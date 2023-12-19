16833d6bd2dc19e2a872c23dce12d301ccdc3170
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-19 14:29:51 -0300

    att

---
+++ exercicio2js-aula14/ex016/script.js
@@ -0,0 +1,10 @@
+function contar() {
+    var inicio = Number(document.querySelector('#inicio'))
+    var fim = Number(document.querySelector('#fim'))
+    var passo = Number(document.querySelector('#passo'))
+    for (; ; )
+    
+
+
+    
+}
