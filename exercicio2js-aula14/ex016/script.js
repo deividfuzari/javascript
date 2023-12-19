554151230839b446e0d3cf9551737516f82a9fe6
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-19 15:06:07 -0300

    exercicio feito

--- exercicio2js-aula14/ex016/script.js
+++ exercicio2js-aula14/ex016/script.js
@@ -1,10 +1,32 @@
 function contar() {
-    var inicio = Number(document.querySelector('#inicio'))
-    var fim = Number(document.querySelector('#fim'))
-    var passo = Number(document.querySelector('#passo'))
-    for (; ; )
-    
+    let ini = document.querySelector('#inicio')
+    let fim = document.querySelector('#fim')
+    let passo = document.querySelector('#passo')
+    let res = document.querySelector('#res')
 
-
-    
+    if (ini.ariaValueMax.length == 0 || fim.ariaValueMax.length == 0 || passo.ariaValueMax.length ==0) {
+        res.innerHTML = 'IMPOSSIVEL CONTAR!'
+        //window.alert('[ERRO] Faltam dados!)
+    }else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Invalido! Considerando Passo 1')
+            p = 1
+        }
+        if (i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c+= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }      
 }
