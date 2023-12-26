d34e00b8e4f98756425ebf5d1d3731a3ca9232d6
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-26 11:36:59 -0300

    aprendendo vetores com JS

---
+++ aula15/ambiente.js
@@ -0,0 +1,6 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1) // adicionar no ultimo elemento
+num.sort()  // colocar em ordem crescente
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
