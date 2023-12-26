5ee491d5adafc9839164fd1f47af30df1b7dbda5
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-26 15:08:43 -0300

    funçoes em JS

---
+++ aula16/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0){ //quando nao é passado todos os paramentros deixar na funcao setado um valor
+    return n1 + n2
+}
+
+console.log(soma(7))    //quando nao é passado todos os paramentros fazer o que foi feito na funcao
---
+++ aula16/funcao03.js
@@ -0,0 +1,5 @@
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5)) //deu um parametro para a funcao dentro de uma variavel
