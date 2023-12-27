ddd4da001778ec9d7c195e54991cfbb9547a0467
Deivid Fuzari <152648702+deividfuzari@users.noreply.github.com>
2023-12-27 13:52:27 -0300

    finalizando o exercicio

--- exercicioaula15-16/ex18/script.js
+++ exercicioaula15-16/ex18/script.js
@@ -22,8 +22,40 @@ function inLista(n, l) {
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     }else {
         window.alert('Valor inválido ou ja encontrado na lista')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+    if(valores.length == 0){
+        window.alert('Adicione valores antes de finalizar!')
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if ( valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+        media = soma / tot
+        res.innerHTML = '' 
+        res.innerHTML += `<p>Ao todo, temos ${tot} de numeros cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>A media dos valores informados é ${media}</p>`
     }
 }
